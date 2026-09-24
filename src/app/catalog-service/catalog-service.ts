21d39370a68fb571c4e0fcb4c2cda1f718c0f1c0
Minh Anh <phamtranminhanh2411@gmail.com>
2026-09-24 14:45:34 +0700

    Complete Bai 14 - Json Array Product Catalog

--- src/app/catalog-service/catalog-service.ts
+++ src/app/catalog-service/catalog-service.ts
@@ -11,21 +11,21 @@ export class CatalogService {
 {"Cateid":"cate1","CateName":"nuoc ngot",
 "Products":[
 {"ProductId":"p1","ProductName":"Coca","Price":100,
-"Image":"assets/h1.webp"},
+"Image":"assets/coca.jpg"},
 {"ProductId":"p2","ProductName":"Pepsi","Price":300,
-"Image":"assets/h2.jpg"},
+"Image":"assets/pepsi.jpg"},
 {"ProductId":"p3","ProductName":"Sting","Price":200,
-"Image":"assets/h3.webp"},
+"Image":"assets/sting.jpg"},
 ]
 },
 {"Cateid":"cate2","CateName":"Bia",
 "Products":[
 {"ProductId":"p4","ProductName":"Heleiken","Price":500,
-"Image":"assets/h4.jpg"},
+"Image":"assets/asset4.jpg"},
 {"ProductId":"p5","ProductName":"333","Price":400,
-"Image":"assets/h5.jpg"},
+"Image":"assets/asset5.jpg"},
 {"ProductId":"p6","ProductName":"Sai Gon","Price":600,
-"Image":"assets/h6.jpg"}
+"Image":"assets/asset6.jpg"}
 ]
 },
 ]
